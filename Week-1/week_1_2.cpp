49700140dce6ebecec994cd60abdc175e31762ba
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-03-25 09:53:29 +0530

    changes in folder structure

--- Week-1/week_1_2.cpp
+++ Week-1/week_1_2.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int binarySearch(int arr[], int l, int r, int x){
+     if (r >= l) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+
 int main()
 {
     int n,size,number; //no. of textcases , size of array and number to be searched
